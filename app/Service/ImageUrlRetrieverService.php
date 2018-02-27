d4b1b4d3ef76a85393ef1d3f13ce7e6c81d12340
lukas <lukas@cvguy.de>
2018-02-27 21:31:14 +0900

    laravel cache of google image search

--- app/Service/ImageUrlRetrieverService.php
+++ app/Service/ImageUrlRetrieverService.php
@@ -14,27 +14,29 @@ class ImageUrlRetrieverService {
     }
 
     public function getImage($name) {
-        // return "https://lumiere-a.akamaihd.net/v1/images/luke-skywalker-main_5a38c454_461eebf5.jpeg?region=0%2C0%2C1536%2C864&width=768";
+        return Cache::remember('image' . $name , 60*24, function() use ($name) {
+            // return "https://lumiere-a.akamaihd.net/v1/images/luke-skywalker-main_5a38c454_461eebf5.jpeg?region=0%2C0%2C1536%2C864&width=768";
 
-        
-        // should cache with laraval but dont know how...
+            
+            // should cache with laraval but dont know how...
 
-        $filename = "/tmp/" . md5($name);
+            //$filename = "/tmp/" . md5($name);
 
-        if(file_exists($filename)) {
-            return file_get_contents($filename);
-        }
+            //if(file_exists($filename)) {
+            //    return file_get_contents($filename);
+            //}
 
-        // just in case for now to save API requests
-        // Luke image response
+            // just in case for now to save API requests
+            // Luke image response
 
-        $service = new \Google_Service_Customsearch($this->client);
-        $optParams = array("cx"=>"011611812558793095754:pehunczquj4", "searchType" => "image");    
-        $results = $service->cse->listCse($name, $optParams);
-        $url = $results->items[0]->link;
+            $service = new \Google_Service_Customsearch($this->client);
+            $optParams = array("cx"=>"011611812558793095754:pehunczquj4", "searchType" => "image");    
+            $results = $service->cse->listCse($name, $optParams);
+            $url = $results->items[0]->link;
 
-        file_put_contents($filename, $url);
+            //file_put_contents($filename, $url);
 
-        return $url;
+            return $url;
+        });
     }
 }
